96fc6e67140434ce94e9a07e7432d8e9e97b7c3b
Nurassyl <vip.nurasyl7@gmail.com>
2022-04-27 21:28:11 +0600

    first commit

---
+++ api_product/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+
+from django.conf.urls import include
+from django.urls import re_path as url
+
+from api_product import views
+from django.contrib.auth.decorators import login_required
+
+urlpatterns = [
+	path('create-cart/<slug>/', views.CreateCart.as_view(), name='api-create-cart'),
+	path('clear/', views.ClearCart.as_view(), name='api-clear'),
+	path('list/', views.ListProduct.as_view() , name='api-list'),
+	path('detail/<slug>/', views.DetailProduct.as_view(), name='api-show'),
+	path('summary/', views.SummaPage.as_view(), name='api-summary'),
+]
